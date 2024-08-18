991406f1a96c1f3db7c85709afe6166ab3b4cb51
peter88213 <petert88213@gmail.com>
2024-08-18 12:24:08 +0200

    Update build script.

--- tools/build_mm2yw7.py
+++ tools/build_mm2yw7.py
@@ -16,10 +16,10 @@ TARGET_FILE = f'{BUILD}mm2yw7.pyw'
 
 
 def main():
-    # inliner.run(SOURCE_FILE, TARGET_FILE, 'mm2yw7lib', '../src/')
-    # inliner.run(TARGET_FILE, TARGET_FILE, 'pywriter', '../src/')
-    inliner.run(SOURCE_FILE, TARGET_FILE, 'mm2yw7lib', '../src/', copyPyWriter=True)
-    inliner.run(TARGET_FILE, TARGET_FILE, 'pywriter', '../../PyWriter/src/', copyPyWriter=True)
+    inliner.run(SOURCE_FILE, TARGET_FILE, 'mm2yw7lib', '../src/')
+    inliner.run(TARGET_FILE, TARGET_FILE, 'pywriter', '../src/')
+    # inliner.run(SOURCE_FILE, TARGET_FILE, 'mm2yw7lib', '../src/', copyPyWriter=True)
+    # inliner.run(TARGET_FILE, TARGET_FILE, 'pywriter', '../../PyWriter/src/', copyPyWriter=True)
     print('Done.')
 
 
